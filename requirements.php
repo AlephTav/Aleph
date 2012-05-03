2cf51ca49a6ffc6a8018cf28c6ac0a0b9691de2c
AlephTav <smilimko@gmail.com>
2012-05-03 12:17:24 +0600

    The first commit.

---
+++ requirements.php
@@ -0,0 +1,6 @@
+<?php
+
+if (version_compare(PHP_VERSION, '5.3.0') < 0) die('<span style="color:#B22222;">Version of your PHP is ' . PHP_VERSION . ', but Aleph requires not less than PHP 5.3.0</span>');
+echo 'All requirements are met.';
+
+?>
