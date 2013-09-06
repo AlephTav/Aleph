01119d8e054c1135f171f6048f521c2e546b8e39
AlephTav <4lephtav@gmail.com>
2013-09-06 16:36:33 +0600

    Working on the documentation of the db layer.

--- lib/db/core/sqlite/sqlitebuilder.php
+++ lib/db/core/sqlite/sqlitebuilder.php
@@ -266,9 +266,9 @@ class SQLiteBuilder extends SQLBuilder
    *
    * @param string $name - the name of the foreign key constraint.
    * @param string $table - the table that the foreign key constraint will be added to.
-   * @param string $columns - the name of the column to that the constraint will be added on. If there are multiple columns, separate them with commas.
+   * @param array $columns - the column(s) to that the constraint will be added on.
    * @param string $refTable - the table that the foreign key references to.
-   * @param string $refColumns - the name of the column that the foreign key references to. If there are multiple columns, separate them with commas.
+   * @param array $refColumns - the column(s) that the foreign key references to.
    * @param string $delete - the ON DELETE option. Most DBMS support these options: RESTRICT, CASCADE, NO ACTION, SET DEFAULT, SET NULL
    * @param string $update - the ON UPDATE option. Most DBMS support these options: RESTRICT, CASCADE, NO ACTION, SET DEFAULT, SET NULL
    * @return string
