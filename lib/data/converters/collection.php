590d8e27f86c3d4b2898b73fe4609cce06707999
AlephTav <4lephtav@gmail.com>
2013-10-27 22:51:24 +0600

    added the Collection converter.

---
+++ lib/data/converters/collection.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Copyright (c) 2012 Aleph Tav
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author Aleph Tav <4lephtav@gmail.com>
+ * @link http://www.4leph.com
+ * @copyright Copyright &copy; 2012 Aleph Tav
+ * @license http://www.opensource.org/licenses/MIT
+ */
+
+namespace Aleph\Data\Converters;
+
+use Aleph\Core;
+
+/**
+ * This converter is intended for converting the given array to an array with another structure. 
+ *
+ * @author Aleph Tav <4lephtav@gmail.com>
+ * @version 1.0.3
+ * @package aleph.data.converters
+ */
+class Collection extends Converter
+{
+  /**
+   * Error message templates.
+   */
+  const ERR_CONVERTER_COLLECTION_1 = 'The given value is not an array.';
+  const ERR_CONVERTER_COLLECTION_2 = 'Invalid conversion mode "[{var}]".';
+  const ERR_CONVERTER_COLLECTION_3 = 'Transformation from "[{var}]" to "[{var}]" is impossible.';
+  const ERR_CONVERTER_COLLECTION_4 = 'Keys "[{var}]" are incorrect.';
+
+  /**
+   * The mode of the array structure converting. The valid values are "transform", "reduce" and "exclude".
+   *
+   * @var string $mode
+   * @access public
+   */
+  public $mode = 'transform';
+  
+  /**
+   * The scheme that describes the new array structure and conversion method.
+   *
+   * @var array $scheme
+   * @access public
+   */
+  public $scheme = [];
+  
+  public $separator = '.';
+  
+  public $anyKeyAssociative = '$';
+  
+  public $anyKeyNumeric = '*';
+  
+  public function convert($entity)
+  {
+    if (!is_array($entity)) throw new Core\Exception($this, 'ERR_CONVERTER_COLLECTION_1');
+    switch (strtolower($this->mode))
+    {
+      case 'transform':
+        return $this->transform($entity);
+      case 'reduce':
+        return $this->reduce($entity);
+      case 'exclude':
+        return $this->exclude($entity);
+    }
+    throw new Core\Exception($this, 'ERR_CONVERTER_COLLECTION_2', $this->mode);
+  }
+  
+  public function transform(array $array)
+  {
+    $new = []; $separator = preg_quote($this->separator, '/');
+    foreach ($this->scheme as $from => $to)
+    {
+      if (is_array($to)) $keys = $to;
+      else
+      {
+        $keys = preg_split('/(?<!\\\)' . $separator . '/', $to, -1, PREG_SPLIT_NO_EMPTY);
+        foreach ($keys as &$part)
+        {
+          if ($part == $this->anyKeyAssociative) $part = ['$'];
+          else if ($part == $this->anyKeyNumeric) $part = ['*'];
+          else if ($part == '\\' . $this->anyKeyAssociative) $part = $this->$this->anyKeyAssociative;
+          else if ($part == '\\' . $this->anyKeyNumeric) $part = $this->$this->anyKeyNumeric;
+          else $part = str_replace('\\' . $this->separator, $this->separator, $part);
+        }
+      }
+      foreach ($this->getValues($array, $from) as $info)
+      {
+        $a = &$new;
+        foreach ($keys as $key)
+        {
+          if (is_array($key))
+          {
+            if (count($info[1]) == 0) throw new Core\Exception($this, 'ERR_CONVERTER_COLLECTION_3', $from, $to);
+            if ($key[0] == '*')
+            {
+              array_shift($info[1]);
+              $key = count($a);
+            }
+            else if ($key[0] == '$')
+            {
+              $key = array_shift($info[1]);
+            }
+          }
+          if (!array_key_exists($key, $a)) $a[$key] = [];
+          $a = &$a[$key];
+        }
+        $a = $info[0];
+      }
+    }
+    return $new;
+  }
+  
+  protected function getValues(array $array, $from, array $keys = null)
+  {
+    if (!$keys) $keys = preg_split('/(?<!\\\)' . preg_quote($this->separator) . '/', $from, -1, PREG_SPLIT_NO_EMPTY);
+    $n = 0;
+    foreach ($keys as $key)
+    {
+      if ($key == $this->anyKeyAssociative)
+      {
+        if (!is_array($array)) throw new Core\Exception($this, 'ERR_CONVERTER_COLLECTION_4', $from);
+        foreach ($array as $k => $v)
+        {
+          if (is_array($v))
+          {
+            foreach ($this->getValues($v, $from, array_slice($keys, $n + 1)) as $value) 
+            {
+              $value[1] = array_merge([$k], $value[1]);
+              yield $value;
+            }
+          }
+          else
+          {
+            yield [$v, [$k]];
+          }
+        }
+        return;
+      }
+      else
+      {
+        if ($key == '\\' . $this->anyKeyAssociative) $key = $this->anyKeyAssociative;
+        else $key = str_replace('\\' . $this->separator, $this->separator, $key);
+        if (!is_array($array) || !array_key_exists($key, $array)) throw new Core\Exception($this, 'ERR_CONVERTER_COLLECTION_4', $from);
+        $array = $array[$key];
+      }
+      $n++;
+    }
+    yield [$array, []];
+  }
+}
